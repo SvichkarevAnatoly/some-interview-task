3f1d1ce294806cfa03c7d1042862cc0525c77559
Svichkarev Anatoly <svichkarev.anatoly@gmail.com>
2018-09-02 22:40:04 +0300

    add hibernate

--- testejb/src/main/java/com/svichkarev/anatoly/HelloBean.java
+++ testejb/src/main/java/com/svichkarev/anatoly/HelloBean.java
@@ -1,12 +1,22 @@
 package com.svichkarev.anatoly;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Stateless
 public class HelloBean implements Hello {
 
+    @PersistenceContext(unitName = "examplePU")
+    private EntityManager entityManager;
+
     @Override
     public String sayHello() {
+        UserEntity userEntity = entityManager.find(UserEntity.class, "1");
+        if (userEntity == null) {
+            entityManager.persist(new UserEntity("1", "Anatoly"));
+        }
+
         return "Hello world from ejb";
     }
 }
