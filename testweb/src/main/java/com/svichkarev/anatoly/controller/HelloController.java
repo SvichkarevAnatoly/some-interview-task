b6e3aaa722ad4ceac57953703af8bc98e5cf237e
Svichkarev Anatoly <svichkarev.anatoly@gmail.com>
2018-09-02 20:51:29 +0300

    client for ejb

--- testweb/src/main/java/com/svichkarev/anatoly/controller/HelloController.java
+++ testweb/src/main/java/com/svichkarev/anatoly/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.svichkarev.anatoly.controller;
 
+import com.svichkarev.anatoly.Hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,24 +8,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Properties;
+
 @Controller
 public class HelloController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String printWelcome(ModelMap model) {
-
-        model.addAttribute("name", "Spring 3400 MVC Hello World");
+    public String printWelcome(ModelMap model) throws NamingException {
+        model.addAttribute("name", getEjbHello());
         return "hello";
-
     }
 
     @RequestMapping(value = "/hello/{name:.+}", method = RequestMethod.GET)
     public ModelAndView hello(@PathVariable("name") String name) {
-
         ModelAndView model = new ModelAndView();
         model.setViewName("hello");
         model.addObject("name", name);
 
         return model;
+    }
+
+    private String getEjbHello() throws NamingException {
+        final Properties jndiProperties = new Properties();
+        jndiProperties.setProperty(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+        InitialContext context = new InitialContext(jndiProperties);
+
+        Hello bean = (Hello) context.lookup("ejb:/testejb//HelloBean!com.svichkarev.anatoly.Hello");
+        return bean.sayHello();
     }
 }
