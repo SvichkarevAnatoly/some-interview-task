5606b3ec7c89811ff74e3de2136f96eb60f631f2
Svichkarev Anatoly <svichkarev.anatoly@gmail.com>
2018-09-11 21:13:44 +0300

    testejb receive saved order id

--- processorejb/src/main/java/com/svichkarev/anatoly/ReceivingMdb.java
+++ processorejb/src/main/java/com/svichkarev/anatoly/ReceivingMdb.java
@@ -2,12 +2,20 @@ package com.svichkarev.anatoly;
 
 import com.svichkarev.anatoly.ejb.EOrder;
 
+import javax.annotation.Resource;
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
+import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.MessageProducer;
 import javax.jms.ObjectMessage;
+import javax.jms.Queue;
+import javax.jms.QueueConnection;
+import javax.jms.QueueSession;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -21,6 +29,12 @@ import static com.svichkarev.anatoly.db.EOrder.toDbEntity;
 })
 public class ReceivingMdb implements MessageListener {
 
+    @Resource(mappedName = "java:/ConnectionFactory")
+    private ConnectionFactory factory;
+
+    @Resource(mappedName = "java:/txresponse")
+    private Queue responseQueue;
+
     @PersistenceContext(unitName = "orderPU")
     private EntityManager entityManager;
 
@@ -29,10 +43,9 @@ public class ReceivingMdb implements MessageListener {
         EOrder order = getOrder(message);
         System.out.println("onMessage(" + order + ")");
 
-        com.svichkarev.anatoly.db.EOrder dbOrder = toDbEntity(order);
-        entityManager.persist(dbOrder);
-        entityManager.flush();
-        System.out.println(dbOrder.getId());
+        com.svichkarev.anatoly.db.EOrder dbOrder = save(order);
+
+        sendResponseOrderId(dbOrder.getId(), message);
     }
 
     private EOrder getOrder(Message message) {
@@ -46,5 +59,25 @@ public class ReceivingMdb implements MessageListener {
             }
         }
         return null;
+    }
+
+    private com.svichkarev.anatoly.db.EOrder save(EOrder order) {
+        com.svichkarev.anatoly.db.EOrder dbOrder = toDbEntity(order);
+        entityManager.persist(dbOrder);
+        entityManager.flush();
+        return dbOrder;
+    }
+
+    private void sendResponseOrderId(long orderId, Message receivedMessage) {
+        try (QueueConnection connection = (QueueConnection) factory.createConnection();
+             QueueSession session = connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+             MessageProducer producer = session.createProducer(responseQueue)) {
+
+            TextMessage orderMsg = session.createTextMessage(String.valueOf(orderId));
+            orderMsg.setJMSCorrelationID(receivedMessage.getJMSMessageID());
+            producer.send(orderMsg);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- testejb/src/main/java/com/svichkarev/anatoly/ejb/OrderServiceBean.java
+++ testejb/src/main/java/com/svichkarev/anatoly/ejb/OrderServiceBean.java
@@ -3,12 +3,16 @@ package com.svichkarev.anatoly.ejb;
 import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
 import javax.jms.MessageProducer;
 import javax.jms.ObjectMessage;
 import javax.jms.Queue;
 import javax.jms.QueueConnection;
+import javax.jms.QueueReceiver;
 import javax.jms.QueueSession;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
@@ -22,7 +26,10 @@ public class OrderServiceBean implements OrderService {
     private ConnectionFactory factory;
 
     @Resource(mappedName = "java:/txrequest")
-    private Queue target;
+    private Queue requestQueue;
+
+    @Resource(mappedName = "java:/txresponse")
+    private Queue responseQueue;
 
     @PersistenceContext(unitName = "orderPU")
     private EntityManager entityManager;
@@ -30,7 +37,7 @@ public class OrderServiceBean implements OrderService {
     @Override
     public void addOrder(EOrder order) {
         System.out.println("addOrder(" + order + ")");
-        sendToProcessorEjb(order);
+        runSavingProcess(order);
     }
 
     @Override
@@ -39,19 +46,20 @@ public class OrderServiceBean implements OrderService {
         return new ArrayList<>(query.getResultList());
     }
 
-    private void sendToProcessorEjb(EOrder order) {
+    private void runSavingProcess(EOrder order) {
+        long orderId = 0;
         try {
             QueueConnection connection = (QueueConnection) factory.createConnection();
+            connection.start();
 
             try {
                 QueueSession session = connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
                 try {
-                    MessageProducer producer = session.createProducer(target);
-                    try {
-                        ObjectMessage message = session.createObjectMessage(order);
-                        producer.send(message);
-                    } finally {
-                        producer.close();
+                    MessageProducer producer = session.createProducer(requestQueue);
+                    ObjectMessage message = send(order, session, producer);
+
+                    if (message != null) {
+                        orderId = syncReceive(session, message.getJMSMessageID());
                     }
                 } finally {
                     session.close();
@@ -62,5 +70,29 @@ public class OrderServiceBean implements OrderService {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+
+        System.out.println("Successfully save order " + order + " with id " + orderId);
+    }
+
+    private long syncReceive(QueueSession session, String correlationId) throws JMSException {
+        final String responseSelector = String.format("JMSCorrelationID='%s'", correlationId);
+        QueueReceiver receiver = session.createReceiver(responseQueue, responseSelector);
+        Message response = receiver.receive();
+
+        long orderId = Long.valueOf(((TextMessage) response).getText());
+        receiver.close();
+
+        return orderId;
+    }
+
+    private ObjectMessage send(EOrder order, QueueSession session, MessageProducer producer) throws JMSException {
+        ObjectMessage message;
+        try {
+            message = session.createObjectMessage(order);
+            producer.send(message);
+        } finally {
+            producer.close();
+        }
+        return message;
     }
 }
